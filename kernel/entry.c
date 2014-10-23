d38a7afaf595b0feb90e973930ac8fb06e68c196
waikeungshen <waikeungshen@gmail.com>
2014-10-23 20:13:12 +0800

    用打印*表示时间中断

--- kernel/entry.c
+++ kernel/entry.c
@@ -12,13 +12,13 @@ int kernel_entry()
 
     console_write_color("Hello, OS kernel!\n", rc_black, rc_green);
     
-    asm volatile ("int $0x3");   
+    asm volatile ("int $0x3");
 
     asm volatile ("sti"); // 开启中断
 
     init_timer(200);
 
     //asm volatile ("int $0x21");   
-    asm volatile ("int $0x3");   
+    //asm volatile ("int $0x3");   
     return 0;
 }
